89a0ddf91dc079e90d6c5d540f965f04b1f88529
kovu1233 <yousif2@hotmail.com>
2020-11-26 03:39:54 +0400

    aa
    aa

--- js/folder.js
+++ js/folder.js
@@ -53,9 +53,12 @@ window.resolveLocalFileSystemURL(cordova.file.dataDirectory, dirEntry, dirError)
 
 //alert (cordova.file.applicationDirectory);
 
+
     	//playAudio("file:///data/user/0/com.adobe.phonegap.app/files/phonegapdevapp/www/tones/rs//Daybreak.mp3");
     	//playAudio("file:///android_assets/www/tones/rs//Daybreak.mp3");
-    	playAudio("tones/rs/Daybreak.mp3");
+    	//playAudio("app/src/main/res/raw/Daybreak.mp3");
+    	var mmm = new Media('cdvfile://localhost/tones/rs/Daybreak.mp3');
+    	mmm.play;
 
        	if(screen.lockOrientation) {
 		    screen.lockOrientation('portrait');
