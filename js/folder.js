092ad5ee10a11cd0e97c24a483e1662b5ed0b016
kovu1233 <yousif2@hotmail.com>
2020-11-29 10:23:02 +0400

    Update folder.js

--- js/folder.js
+++ js/folder.js
@@ -66,12 +66,13 @@ alert (getExternalDirectory);*/
 
            //alert(cordova.file);
              
-            var myPath = window.location+"";
+            /*var myPath = window.location+"";
             myPath = myPath.substring(0, myPath.lastIndexOf('/'));
 
 			playAudio(myPath+"/tones/rs/Daybreak.mp3");
-			document.getElementById("alertedContent").value = myPath+"/tones/rs/Daybreak.mp3";
-		///////////playAudio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3");
+			document.getElementById("alertedContent").value = myPath+"/tones/rs/Daybreak.mp3";*/
+
+		playAudio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3");
 
 
     	//playAudio("file:///data/user/0/com.adobe.phonegap.app/files/phonegapdevapp/www/tones/rs//Daybreak.mp3");
