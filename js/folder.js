b437e45f7e6828e81eec265e46b8057c44ef1e58
kovu1233 <yousif2@hotmail.com>
2020-12-02 08:40:14 +0400

    Update folder.js

--- js/folder.js
+++ js/folder.js
@@ -65,7 +65,7 @@ var app = {
 		                   //fileStr += (entries[i].fullPath + "<br>"); // << replace with something useful
 		                   ////////fileStr += ('<tr><td class="tabs1" id="'+entries[i]+'" onclick="AlarmsCFile(this.id);">'+entries[i].fullPath+'</td></tr>');
 		                	//if (entries[i].substr(entries[i].length - 3) == "mp3" || entries[i].substr(entries[i].length - 3) == "ogg"){
-		                		var ext = entries[i].substr(entries[i].length - 3);
+		                		var ext = entries[i].substr(-3);
 								fileStr += ('<tr><td class="tabs1" id="'+entries[i]+'" onclick="AlarmsCFile(this.id);">'+ext+'</td></tr>');		                   	
 		                   		index++;
 		                   	//}
